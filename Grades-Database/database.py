e8afe7e0461fafb2cd8ef4a42f6e6a56ce79ce58
Wasif Khan <wasif.k1112@gmail.com>
2020-02-13 03:24:10 -0500

    Create database.py

---
+++ Grades-Database/database.py
@@ -0,0 +1,16 @@
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="BAAdmin",
+  passwd="password",
+  database="BADatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("""CREATE TABLE students (
+                      id            INT AUTO_INCREMENT PRIMARY KEY, 
+                      name          VARCHAR(255)
+                     );""")                

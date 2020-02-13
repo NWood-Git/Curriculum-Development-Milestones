7fa54ef8fadb314e8f1e9d3ccde542efd2a43459
Wasif Khan <wasif.k1112@gmail.com>
2020-02-13 03:25:39 -0500

    Create grades_introduction_to_python.py

---
+++ Grades-Database/grades_introduction_to_python.py
@@ -0,0 +1,71 @@
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="BAAdmin",
+  passwd="password",
+  database="BADatabase"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("""CREATE TABLE grades_hello_world (
+                      stud_id           INTEGER PRIMARY KEY,
+                      arithmetic        INTEGER,
+                      name_bindings     INTEGER,
+                      operators         INTEGER,
+                      python_caches     INTEGER,
+                      string_arithmetic INTEGER,
+                      user_input        INTEGER,
+                      FOREIGN KEY   (stud_id)
+                        REFERENCES  students (id) 
+                     );""")
+                     
+mycursor.execute("""CREATE TABLE grades_data_types (
+                      stud_id           INTEGER PRIMARY KEY,
+                      arithmetic        INTEGER,
+                      name_bindings     INTEGER,
+                      operators         INTEGER,
+                      python_caches     INTEGER,
+                      string_arithmetic INTEGER,
+                      user_input        INTEGER,
+                      FOREIGN KEY   (stud_id)
+                        REFERENCES  students (id) 
+                     );""")
+
+mycursor.execute("""CREATE TABLE grades_control_flow (
+                      stud_id           INTEGER PRIMARY KEY,
+                      arithmetic        INTEGER,
+                      name_bindings     INTEGER,
+                      operators         INTEGER,
+                      python_caches     INTEGER,
+                      string_arithmetic INTEGER,
+                      user_input        INTEGER,
+                      FOREIGN KEY   (stud_id)
+                        REFERENCES  students (id) 
+                     );""")
+
+mycursor.execute("""CREATE TABLE grades_functions (
+                      stud_id           INTEGER PRIMARY KEY,
+                      arithmetic        INTEGER,
+                      name_bindings     INTEGER,
+                      operators         INTEGER,
+                      python_caches     INTEGER,
+                      string_arithmetic INTEGER,
+                      user_input        INTEGER,
+                      FOREIGN KEY   (stud_id)
+                        REFERENCES  students (id) 
+                     );""")
+                     
+mycursor.execute("""CREATE TABLE grades_complexity_theory (
+                      stud_id           INTEGER PRIMARY KEY,
+                      arithmetic        INTEGER,
+                      name_bindings     INTEGER,
+                      operators         INTEGER,
+                      python_caches     INTEGER,
+                      string_arithmetic INTEGER,
+                      user_input        INTEGER,
+                      FOREIGN KEY   (stud_id)
+                        REFERENCES  students (id) 
+                     );""")                     

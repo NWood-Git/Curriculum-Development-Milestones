c89839efb741199799476a0217bfa9409a75bcce
Wasif Khan <wasif.k1112@gmail.com>
2020-02-23 17:15:25 -0500

    Update grades_software_theory.py

--- Grades-Database/grades_software_theory.py
+++ Grades-Database/grades_software_theory.py
@@ -63,30 +63,6 @@ mycursor.execute("""CREATE TABLE grades_networks (
                         REFERENCES  students (id) 
                      );""")
 
-mycursor.execute("""CREATE TABLE grades_control_flow (
-                      stud_id   INTEGER PRIMARY KEY,
-                      easy_e1   INTEGER,
-                      easy_e2   INTEGER,
-                      easy_e3   INTEGER,
-                      easy_e4   INTEGER,
-                      easy_e5   INTEGER,
-                      easy_e6   INTEGER,
-                      easy_e7   INTEGER,
-                      easy_e8   INTEGER,
-                      easy_e9   INTEGER,
-                      med_e1    INTEGER,
-                      med_e2    INTEGER,
-                      med_e3    INTEGER,
-                      med_e4    INTEGER,
-                      med_e5    INTEGER,
-                      med_e6    INTEGER,
-                      hard_e1   INTEGER,
-                      hard_e2   INTEGER,
-                      hard_e3   INTEGER,
-                      FOREIGN KEY   (stud_id)
-                        REFERENCES  students (id) 
-                     );""")
-
 mycursor.execute("""CREATE TABLE grades_databases (
                       stud_id   INTEGER PRIMARY KEY,
                       easy_e1   INTEGER,

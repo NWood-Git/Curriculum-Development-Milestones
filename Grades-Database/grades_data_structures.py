3d7953d57bd75e0f50f278852bcc3424b6106289
NWood-Git <57541008+NWood-Git@users.noreply.github.com>
2020-02-24 11:53:19 -0500

    Update grades_data_structures.py

--- Grades-Database/grades_data_structures.py
+++ Grades-Database/grades_data_structures.py
@@ -10,10 +10,6 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("""CREATE TABLE students (
-                      id            INT AUTO_INCREMENT PRIMARY KEY, 
-                      name          VARCHAR(255)
-                     );""")
 
 mycursor.execute("""CREATE TABLE grades_complex_data (
                       stud_id   INTEGER PRIMARY KEY,
